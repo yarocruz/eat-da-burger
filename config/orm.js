d6dafbfef306f02eea610f46a0c550850bd7acf8
yarocruz <yarocruz@gmail.com>
2020-03-29 15:44:36 -0400

    created the insertOne and updateOne methods to the ORM object

--- config/orm.js
+++ config/orm.js
@@ -11,4 +11,24 @@ const orm = {
             callback(result);
         })
     },
+    insertOne: function (table, cols, val, callback) {
+        let queryString = `INSERT INTO ${table} (${cols.toString()}) VALUES (${val});`
+        connection.query(queryString, (err, result) => {
+            if (err) {
+                throw err;
+            }
+            callback(result);
+        })
+    },
+    updateOne: function (table, objColVals, condition, callback) {
+        let queryString = `UPDATE ${table} SET ${objColVals} WHERE ${condition};`
+        connection.query(queryString, (err, result) => {
+            if (err) {
+                throw err;
+            }
+            callback(result);
+        })
+    },
 }
+
+module.exports = orm;
