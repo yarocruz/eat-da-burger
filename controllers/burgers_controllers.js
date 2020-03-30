7b6e7ab86c31d41badc322720777c84faa636d4e
yarocruz <yarocruz@gmail.com>
2020-03-30 11:21:54 -0400

    added code to handlebars files and used the render method to display from index

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -6,7 +6,8 @@ const burger = require('../models/burger');
 
 router.get('/', (req, res) => {
     burger.all(data => {
-        res.send(data); // This is just to test out if everything is hooked up together. / Successfully doing the query to SQL Database and returning the data as JSON
+        //res.send(data); // This is just to test out if everything is hooked up together. / Successfully doing the query to SQL Database and returning the data as JSON
+        res.render('index', { burger: data })
     })
 })
 
